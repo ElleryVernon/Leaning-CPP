6d106281d069393e0b8e54bcb45199c4891fb0d7
Han Seong-Min <tmpks3@gmail.com>
2022-07-01 23:09:26 +0900

    update 001_HelloWorld.cpp

--- 001_HelloWorld.cpp
+++ 001_HelloWorld.cpp
@@ -7,8 +7,7 @@ int main(void)
     int num = 20;
 
     cout << "Hello World!" << endl;
-    cout << "Hello "
-         << "World!" << endl;
+    cout << "Hello " << "World!" << endl;
     cout << num << ' ' << 'A';
     cout << ' ' << 3.14 << endl;
 }
